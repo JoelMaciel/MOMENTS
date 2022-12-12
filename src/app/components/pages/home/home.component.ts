5e0bee9f29509ee5dc8b02c64988a50457417876
Joel Maciel <soldado25461@gmail.com>
2022-12-12 14:49:52 -0300

    feat: fetching data from the database to the Home page

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
-  }
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
 
+      data.map((items) => {
+        items.created_at = new Date(items.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
