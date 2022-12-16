bc21a5e51216bfecd8b0df29c2d8f6fab7a2d2dd
Joel Maciel <soldado25461@gmail.com>
2022-12-16 13:42:53 -0300

    feat: data search by title

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,6 +14,9 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
 
+  faSearch = faSearch;
+  searchTerm: string = '';
+
   constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
@@ -28,6 +31,15 @@ export class HomeComponent implements OnInit {
 
       this.allMoments = data;
       this.moments = data;
+    });
+  }
+
+  search(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value);
     });
   }
 }
